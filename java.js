9c8f1f85cef9bb5d6171f1f3892ec14ad4e62fc4
lab_web <168737808+JhonnnyTzy@users.noreply.github.com>
2024-05-09 05:47:44 -0400

    Actualizando estilos CSS, JavaScript y HTML

--- java.js
+++ java.js
@@ -1,20 +1,12 @@
-const images =document.querySelectorAll('.image');
-const modal= document.getElementById('modal');
-const modalDescription= document.getElementById('modal-description');
+const images = document.getElementsByClassName('image');
+const modal = document.getElementById('modal');
+const modalDescription = document.getElementById('modal-description');
 
-images.forEach(Image=>{
-    Image.addEventListener('click', () =>{
-        modalDescription.textContent=Image.dataset.description;
-        modal.style.display='block';
-    });
-});
-
-document.querySelector('.close').addEventListener('click', () =>{
-    modal.style.display='none';
-});
-
-window.addEventListener('click', (event)=>{
-    if (event.target===modal) {
-        modal.style.display='none';
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('image')) {
+        modalDescription.textContent = event.target.dataset.description;
+        modal.style.display = 'block';
+    } else if (event.target.classList.contains('close') || event.target === modal) {
+        modal.style.display = 'none';
     }
 });
